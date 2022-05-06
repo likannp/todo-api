f8c9352b074bfaf23b0df62c95178604f38fa289
Elisangela <elisangela00silva@gmail.com>
2022-05-06 15:35:59 -0300

    index

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,17 @@
+class ProjectsController < ApplicationController
+  def index
+    @Project = Project.all
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
